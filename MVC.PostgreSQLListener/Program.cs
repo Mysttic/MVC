90e4b29d4bea06f2f784a91b50281a2de9ac6f64
Jakub Kamiński <jakub.kaminski@intelimedical.pl>
2025-02-17 11:18:30 +0100

    Add Windows Service support for non-container environments

--- MVC.PostgreSQLListener/Program.cs
+++ MVC.PostgreSQLListener/Program.cs
@@ -1,10 +1,49 @@
-﻿internal class Program
+﻿using System.ServiceProcess;
+
+internal class Program
 {
 	private static async Task Main(string[] args)
 	{
-		Settings settings = new Settings().FillParameters(args);
-		IListener listener = new PostgreSQLListener(settings);
-		AppDomain.CurrentDomain.ProcessExit += (s, e) => listener.Stop();
-		listener.Start();
+		// Sprawdź, czy aplikacja działa w kontenerze
+		if (Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") == "true")
+		{
+			// Uruchom logikę bez ServiceBase (tryb konsolowy)
+			Settings settings = new Settings().FillParameters(args);
+			IListener listener = new PostgreSQLListener(settings);
+			Console.CancelKeyPress += (s, e) => listener.Stop();
+			listener.Start();
+
+			// Utrzymaj proces w działaniu
+			await Task.Delay(-1);
+		}
+		else
+		{
+			ServiceBase.Run(new MyService());
+		}
+	}
+
+	internal class MyService : ServiceBase
+	{
+		private IListener _listener;
+
+		public MyService()
+		{
+			this.ServiceName = "MVC.PostgreSQLListenerService";
+		}
+
+		protected override void OnStart(string[] args)
+		{
+			Task.Run(() =>
+			{
+				Settings settings = new Settings().FillParameters(args);
+				_listener = new PostgreSQLListener(settings);
+				_listener.Start();
+			});
+		}
+
+		protected override void OnStop()
+		{
+			_listener?.Stop();
+		}
 	}
 }
